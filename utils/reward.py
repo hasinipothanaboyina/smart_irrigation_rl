57c862e4a4f347156e4b45bc8c4165b02df612e3
hasinipothanaboyina <hasinipothanaboyina0@gmail.com>
2026-04-07 21:50:09 +0530

    Delete utils directory

--- utils/reward.py
+++
@@ -1,7 +0,0 @@
-def compute_reward(soil):
-    if 40 <= soil <= 70:
-        return 10, "Optimal Moisture"
-    elif soil < 40:
-        return -5, "Underwatered"
-    else:
-        return -7, "Overwatered"
